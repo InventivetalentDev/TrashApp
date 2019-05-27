88f9ecc1d8c45e5748ec451359dd32a11ecac0d5
inventivetalent <mail@inventivetalent.org>
2019-05-27 21:53:11 +0200

    change to MultiDexApp

---
+++ mobile/src/main/java/org/inventivetalent/trashapp/TrashApp.java
@@ -0,0 +1,11 @@
+package org.inventivetalent.trashapp;
+
+import androidx.multidex.MultiDexApplication;
+
+public class TrashApp extends MultiDexApplication {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
