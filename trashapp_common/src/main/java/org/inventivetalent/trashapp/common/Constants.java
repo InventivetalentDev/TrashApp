dfa9e963f552431ae2450027e0f5dd7b2c96d68e
inventivetalent <mail@inventivetalent.org>
2019-05-29 11:28:03 +0200

    increase update intervals

--- trashapp_common/src/main/java/org/inventivetalent/trashapp/common/Constants.java
+++ trashapp_common/src/main/java/org/inventivetalent/trashapp/common/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
 	public static final int REQUEST_LOCATION_PERMS_CODE = 5589;
 	public static final int OSM_REQUEST_CODE            = 5588;
 
-	public static final long  LOCATION_REFRESH_TIME     = 10000;// milliseconds
-	public static final float LOCATION_REFRESH_DISTANCE = 0.5f;// meters
+	public static final long  LOCATION_REFRESH_TIME     = 2000;// milliseconds
+	public static final float LOCATION_REFRESH_DISTANCE = 0.2f;// meters
 
 	public static final int DEFAULT_SEARCH_RADIUS = 5000;// meters
 	public static final int SEARCH_STEP           = 1000;
