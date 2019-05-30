95cfc4f1e21dca27d80696a6f1fb99fffea6c1fe
inventivetalent <mail@inventivetalent.org>
2019-05-30 12:44:45 +0200

    replace the premium thingy with a patreon link

--- mobile/src/main/java/org/inventivetalent/trashapp/AboutActivity.java
+++ mobile/src/main/java/org/inventivetalent/trashapp/AboutActivity.java
@@ -1,13 +1,14 @@
 package org.inventivetalent.trashapp;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import org.inventivetalent.trashapp.common.Util;
@@ -46,11 +47,13 @@ public class AboutActivity extends AppCompatActivity {
 		button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (TabActivity.SKU_INFO_PREMIUM != null) {
-					TabActivity.SKU_INFO_PREMIUM.launchBilling();
-				} else {
-					Toast.makeText(AboutActivity.this, "Product not ready!", Toast.LENGTH_SHORT).show();
-				}
+//				if (TabActivity.SKU_INFO_PREMIUM != null) {
+//					TabActivity.SKU_INFO_PREMIUM.launchBilling();
+//				} else {
+//					Toast.makeText(AboutActivity.this, "Product not ready!", Toast.LENGTH_SHORT).show();
+//				}
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.patreon.com/inventivetalent"));
+				startActivity(browserIntent);
 			}
 		});
 	}
