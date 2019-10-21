fe4465e120b9319461040a4610ce6ec167a77896
inventivetalent <mail@inventivetalent.org>
2019-10-21 09:07:07 +0200

    cleanup

--- mobile/src/main/java/org/inventivetalent/trashapp/AboutActivity.java
+++ mobile/src/main/java/org/inventivetalent/trashapp/AboutActivity.java
@@ -1,12 +1,9 @@
 package org.inventivetalent.trashapp;
 
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
