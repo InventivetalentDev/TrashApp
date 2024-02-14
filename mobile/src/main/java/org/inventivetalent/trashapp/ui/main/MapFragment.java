e562f53d6194d9604e47b4c4050fcaa8ef8bd9f0
Haylee Schäfer <mail@inventivetalent.org>
2024-02-14 09:38:35 +0100

    switch to tile.openstreetmap.org https://github.com/geopython/pygeoapi/pull/697#issuecomment-850688337

--- mobile/src/main/java/org/inventivetalent/trashapp/ui/main/MapFragment.java
+++ mobile/src/main/java/org/inventivetalent/trashapp/ui/main/MapFragment.java
@@ -55,7 +55,7 @@ public class MapFragment extends Fragment {
 
 	public static final OnlineTileSourceBase WIKIMAPS = new XYTileSource("WikimediaMaps",
 			0, 19, 256, ".png", new String[] {
-			"https://maps.wikimedia.org/osm-intl/" }, "© OpenStreetMap contributors",
+			"https://tile.openstreetmap.org/" }, "© OpenStreetMap contributors",
 			new TileSourcePolicy(2,
 					TileSourcePolicy.FLAG_NO_BULK
 							| TileSourcePolicy.FLAG_NO_PREVENTIVE
